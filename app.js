5538358a16db03dfcdf3e2308f97962cb36192a5
unknown <ryan20bv@gmail.com>
2023-07-12 10:34:28 +0800

    create a route

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const app = express();
+
 const PORT = process.env.PORT;
 const ConnectMongoDB = require("./databaseConnection/ConnectMongoDB");
 
+const app = express();
+
+// // games router
+const GamesRouter = require("./mongoDBSetUp/routes/game-routes");
+app.use("/api/tiktaktoe", GamesRouter);
+
 // for testing
 app.use((req, res, next) => {
-	res.send("Hello World 123!");
+	res.send("Hello Tik Tak Toe World!");
 });
 
 app.listen(PORT, async () => {
